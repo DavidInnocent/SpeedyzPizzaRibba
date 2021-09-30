5e56d9dc115642e15577381305146b85df251b34
David Innocent <davidinnocent@live.com>
2021-09-30 16:22:58 +0300

    retrieved all pizzas,toppings and discounts for configurator

---
+++ src/app/order/configurator/configurator.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ConfiguratorService } from './configurator.service';
+
+interface Pizza{
+  Name: string,
+  Price: number
+}interface Topping{
+  Name: string,
+  Price: number
+}interface Discount{
+  Name: string,
+  Amount: number
+}
+@Component({
+  selector: 'app-configurator',
+  templateUrl: './configurator.component.html',
+  styleUrls: ['./configurator.component.scss']
+})
+export class ConfiguratorComponent implements OnInit {
+
+  pizzas:Array<Pizza>;
+  toppings:Array<Topping>;
+  discounts:Array<Discount>;
+  constructor(configurator:ConfiguratorService) {
+    configurator.pizzas.subscribe(pizzasReturned=>this.pizzas=pizzasReturned);
+    configurator.toppings.subscribe(toppingsReturned=>this.toppings=toppingsReturned);
+    configurator.discounts.subscribe(discountsReturned=>this.discounts=discountsReturned);
+  
+    
+   }
+
+  ngOnInit(): void {
+    this.pizzas.forEach(pizza=>console.log(pizza.Name))
+    }
+
+}
---
+++ src/app/order/configurator/configurator.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfiguratorService {
+  toppings: Observable<any[]>;
+  pizzas: Observable<any[]>;
+  discounts: Observable<any[]>;
+
+  constructor(firestore: AngularFirestore) {
+    this.toppings = firestore.collection('Toppings').valueChanges();
+    this.pizzas = firestore.collection('Pizzas').valueChanges();
+    this.discounts = firestore.collection('Discounts').valueChanges();
+  }
+}
