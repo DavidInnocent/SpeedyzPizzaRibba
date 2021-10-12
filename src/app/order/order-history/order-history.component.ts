0dc319ecef4bc42c154faf4239854169c18d7999
David Innocent <davidinnocent@live.com>
2021-10-12 22:56:04 +0300

    added reorder functionality

--- src/app/order/order-history/order-history.component.ts
+++ src/app/order/order-history/order-history.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Order } from 'src/app/models/order';
+import { DataSharingService } from 'src/app/shared/services/data-sharing.service';
 import { OrderHistoryService } from './order-history.service';
 
 @Component({
@@ -8,10 +11,15 @@ import { OrderHistoryService } from './order-history.service';
 })
 export class OrderHistoryComponent implements OnInit {
 
-  
-  constructor(public orderService:OrderHistoryService) { }
+
+  constructor(public orderService:OrderHistoryService,public dataSharingService:DataSharingService,public router:Router) { }
 
   ngOnInit(): void {
+  }
+  ReOrder(order:Order){
+    order.DiscountApplied=0;
+    this.dataSharingService.setOrder(order);
+    this.router.navigateByUrl('order_details');
   }
 
 }
